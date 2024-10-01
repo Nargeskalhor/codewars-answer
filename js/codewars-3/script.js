68dad496b50b4c84fdeccff31554b0a71c47336a
narges <13.narges.kalhor.82@gmail.com>
2024-10-02 00:46:07 +0330

    Reversed Words

--- js/codewars-3/script.js
+++ js/codewars-3/script.js
@@ -1,24 +1,17 @@
 "use strict";
-// function points(games) {
-//   let total = 0;
-
-//   let x;
-//   let y;
-//   if (x > y) {
-//     total += 3;
-//   } else if ((x = y)) {
-//     total += 1;
-//   }
-//   return total;
-// }
-// console.log(points(["3:1", "3:2", "0:1"]));
-
 function points(games) {
-  let sum = 0;
-  for (let i = 0; i < games.length; i++) {
-    if (games[i][x] > games[i][y]) sum += 3;
-    else if (games[i][x] == games[i][y]) sum += 1;
+  return games;
+  let total = 0;
+  games.forEach((games) => {
+    let [x, y] = games.split(":").map(Number);
+  });
+
+  if (x > y) {
+    total += 3;
+  } else if ((x = y)) {
+    total += 1;
+  } else {
+    return total;
   }
-  return sum;
+  console.log(points(["3:1", "3:2", "0:1"]));
 }
-console.log(points(["3:1", "2:2", "0:1"]));
---
+++ js/codewars-5/script.js
@@ -0,0 +1,5 @@
+"use strict";
+function reverseWords(str) {
+  return str.split(" ").reverse().join(" ");
+}
+console.log("narges kalhor");
